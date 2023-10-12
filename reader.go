f13e16063daee6b20d2bc7d6c5925b83e83957c2
laotie <flaboy.cn@gmail.com>
2023-10-12 17:25:31 +0800

    m

--- reader.go
+++ reader.go
@@ -64,48 +64,58 @@ func (me *parser) full() {
 
 		if s, ok := me.set[key]; strings.ToUpper(f.Name[0:1]) == f.Name[0:1] && ok {
 			fv := v.Field(i)
-
-			switch fv.Type().Kind() {
-			case reflect.String:
-				fv.SetString(s)
-			case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int8:
-				x, err := strconv.ParseInt(s, 10, 0)
-				if err != nil {
-					log.Printf("config error: %s required int, %s given\n", key, s)
-				} else {
-					fv.SetInt(x)
-				}
-			case reflect.Uint, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint8:
-				x, err := strconv.ParseUint(s, 10, 0)
-				if err != nil {
-					log.Printf("config error: %s required uint, %s given\n", key, s)
-				} else {
-					fv.SetUint(x)
-				}
-			case reflect.Bool:
-				switch s {
-				case "yes", "on":
-					fv.SetBool(true)
-				default:
-					b, _ := strconv.ParseBool(s)
-					fv.SetBool(b)
-				}
-			case reflect.Float32, reflect.Float64:
-				x, err := strconv.ParseFloat(s, 0)
-				if err != nil {
-					log.Printf("config error: %s required float, %s given\n", key, s)
-				} else {
-					fv.SetFloat(x)
-				}
-			default:
-				obj := reflect.New(fv.Type()).Interface()
-				err := json.Unmarshal([]byte(s), obj)
-				if err != nil {
-					log.Printf("config error: %s required %s, JSON Error: %s\n", key, fv.Type().Kind(), err)
-				} else {
-					fv.Set(reflect.ValueOf(obj).Elem())
-				}
+			me.setValue(fv, key, s)
+		} else {
+			dft := f.Tag.Get("default")
+			if dft != "" {
+				fv := v.Field(i)
+				me.setValue(fv, key, dft)
 			}
+		}
+	}
+}
+
+func (me *parser) setValue(fv reflect.Value, key, s string) {
+
+	switch fv.Type().Kind() {
+	case reflect.String:
+		fv.SetString(s)
+	case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int8:
+		x, err := strconv.ParseInt(s, 10, 0)
+		if err != nil {
+			log.Printf("config error: %s required int, %s given\n", key, s)
+		} else {
+			fv.SetInt(x)
+		}
+	case reflect.Uint, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint8:
+		x, err := strconv.ParseUint(s, 10, 0)
+		if err != nil {
+			log.Printf("config error: %s required uint, %s given\n", key, s)
+		} else {
+			fv.SetUint(x)
+		}
+	case reflect.Bool:
+		switch s {
+		case "yes", "on":
+			fv.SetBool(true)
+		default:
+			b, _ := strconv.ParseBool(s)
+			fv.SetBool(b)
+		}
+	case reflect.Float32, reflect.Float64:
+		x, err := strconv.ParseFloat(s, 0)
+		if err != nil {
+			log.Printf("config error: %s required float, %s given\n", key, s)
+		} else {
+			fv.SetFloat(x)
+		}
+	default:
+		obj := reflect.New(fv.Type()).Interface()
+		err := json.Unmarshal([]byte(s), obj)
+		if err != nil {
+			log.Printf("config error: %s required %s, JSON Error: %s\n", key, fv.Type().Kind(), err)
+		} else {
+			fv.Set(reflect.ValueOf(obj).Elem())
 		}
 	}
 }
